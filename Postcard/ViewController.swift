82f0525283ef8dd10e7f0dc2f7dca70211e90e4f
Adam McKenna <amckenna@Adams-MacBook-Pro.local>
2014-10-29 16:57:01 -0700

    added name field updates

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //Labels
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    //Text Fields
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
+    
+    //Buttons
     @IBOutlet weak var mailButton: UIButton!
+
     
     
     
@@ -33,8 +40,17 @@ class ViewController: UIViewController {
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
+        
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
+        
+        
         
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
         
